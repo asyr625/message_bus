d2b3c76047e4fda1921073228b072fd3d5ee3bfd
= <619695356@qq.com>
2015-12-03 17:20:49 +0800

    lambda parameter *_*||

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # message_bus
+
+update g++ 4.8 to g++-5 ( g++ 5.2)
+
+cmd.wrap([](int n){ return n+1;}, 1); //1
--- common_command.h
+++ common_command.h
@@ -8,29 +8,35 @@ template<typename R = void>
 struct Common_Command
 {
 public:
-    template<class F, class... Args, class = typename std::enable_if<!std::is_member_function_pointer<F>::value>::type>
-    void warp(F && f, Args && ... args)
+    template<class F, class... Args, class = typename std::enable_if<!std::is_member_function_pointer<F>::value>::type> //
+    void wrap(F && f, Args && ... args)
     {
         //m_f = [&]{ return f(args...); };
-        m_f = std::bind([=](F && f1, Args && ... ar){ return f1(ar...);}, f, args...);
+        //m_f = std::bind([=](F && f1, Args && ... ar){ return f1(ar...);}, f, args...);
         //m_f = [&, args]{ return f(args...); };
+
+        m_f = std::bind(f, args...);
     }
 
-    //template<class R, class C, class ... DArgs, class P, class... Args>
-    template<class C, class ... DArgs, class P, class... Args>
-    void warp(R(C::*f)(DArgs...) const, P && p, Args && ... args)
-    {
-        //m_f = [&, f]{ return (*p.*f)(args...); };
 
-        m_f = std::bind([=](R(C::*f1)(DArgs...) const, P && p1, Args && ... args1){ return (*p1.*f1)(args1...);}, f, p, args...);
+    //template<class C, class... DArgs, class P, class... Args>
+    template<class R1, class C, class... DArgs, class P, class... Args>
+    void wrap(R1(C::*f)(DArgs...), P && p, Args && ... args)
+    {
+        m_f = [&, f]{return (*p.*f)(args...); };
+        //m_f = std::bind([=](R1(C::*f1)(DArgs...), P && p1, Args && ... args1){return (*p1.*f1)(args1...);}, f, p, args...);
     }
 
-    //template<class R, class C, class... DArgs, class P, class... Args>
-    template<class C, class... DArgs, class P, class... Args>
-    void wrap(R(C::*f)(DArgs...), P && p, Args && ... args)
+
+    //template<class C, class ... DArgs, class P, class... Args>
+    template<class R1, class C, class ... DArgs, class P, class... Args>
+    void wrap(R1(C::*f)(DArgs...) const, P && p, Args && ... args)
     {
-        //m_f = [&, f]{return (*p.*f)(args...); };
-        m_f = std::bind([=](R(C::*f1)(DArgs...), P && p1, Args && ... args1){return (*p1.*f1)(args1...);}, f, p, args...);
+        m_f = [&, f]{ return (*p.*f)(args...); };
+
+        //m_f = std::bind([=](R1(C::*f1)(DArgs...) const, P && p1, Args && ... args1){ return (*p1.*f1)(args1...);}, f, p, args...);
+        //m_f = std::bind( f, args...);
+
     }
 
     R execute()
